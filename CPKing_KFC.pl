f2a4eeca92e8c472b7b474c34f3855301b61093f
我 <agudoe2 atraintime2reg@gmail.com>
2017-12-24 13:12:24 +0800

    新增合併功能。目前提供以下，可在req*.txt中啟用：   合併炸烤雞：咔啦脆雞(中辣)_或_上校薄脆雞_或_美式BBQ醬烤煙燻雞_或_醬香酸甜風味炸雞   合併冷飲：百事可樂_或_冰紅茶_或_冰無糖茉莉綠茶 更多註釋 修正讀kfc_coupon.csv時遇到的crlf問題 調整output.html格式。新列所有單點項。 調整排序：價格相同時再比隱藏溢價

--- CPKing_KFC.pl
+++ CPKing_KFC.pl
@@ -14,12 +14,18 @@
 
 # 請注意！需求清單改放到 req.txt 了！
 
+#use strict;
+#use warnings;
+
 
 my $coupon_txt = "kfc_coupon.txt";
 my $output_html = "output.html";
 my $stime = time;
 my ($sec, $min, $hour, $d, $m, $y) = localtime(time);
 my $today_date= (1900+$y)*10000 + ($m+1)*100 + $d;
+my $merge_words_chicken = "合併炸烤雞：咔啦脆雞(中辣)_或_上校薄脆雞_或_美式BBQ醬烤煙燻雞_或_醬香酸甜風味炸雞";
+my $merge_words_coketea = "合併冷飲：百事可樂_或_冰紅茶_或_冰無糖茉莉綠茶";
+my $merge_words_warning = "請注意！可能依不同分店或不同優惠券而有不同結果！不一定可互換成功！";
 
 print "\n";
 print "用法： perl CPKing_KFC.pl [需求表。沒提供的話直接用req.txt]\n";
@@ -65,7 +71,6 @@ while(my $inbuf = <FIN>) {
         next if( $tmp[$item_loc{'優惠價'}] =~ /^\s*$/ );
         next if( &expired( $tmp[$item_loc{'有效期間'}] ) );
 
-        #print "DEBUG: $coupon_idx : $inbuf\n";
         for(my $i=0; $i< @tmp; $i++) {
             $coupon[$coupon_idx][$i] = $tmp[$i];
         }
@@ -73,9 +78,9 @@ while(my $inbuf = <FIN>) {
     } elsif( $inbuf =~ /^名稱/ ) {
         @item_name = split /,/, $inbuf;
         $item_max = @item_name;
-        for(my $i=0; $i< @item_name; $i++) {
+
+        for(my $i=0; $i< $item_max; $i++) {
             $item_loc{ $item_name[$i] } = $i;
-            #print "DEBUG: item_log { $item_name[$i] } = $i\n";
         }
 
         #一定要有的欄位
@@ -89,17 +94,44 @@ while(my $inbuf = <FIN>) {
 
         #處理單價列 20171211
         $ori_price_line = <FIN>;
+        $ori_price_line =~ s/\r?\n$//;
     } elsif( $inbuf =~ /^上次更新/ ) {
         my @tmp = split /,/, $inbuf;
         $csv_ver = $tmp[1];
     }
 }
+close(FIN);
+
+
+#單價列放最後才能補小細縫
+my @ori_price_tmp = split /,/, $ori_price_line;
+for(my $i=$item_1st_loc; $i<$item_max; $i++) {
+    $coupon[$coupon_idx][$item_loc{'名稱'}] = $item_name[$i];
+    $coupon[$coupon_idx][$item_loc{'優惠代碼'}] = "單點";
+    $coupon[$coupon_idx][$item_loc{'連結'}] = "na";
+    $coupon[$coupon_idx][$item_loc{'有效期間'}] = "na";
+    $coupon[$coupon_idx][$item_loc{'原價'}] = $ori_price_tmp[$i];
+    $coupon[$coupon_idx][$item_loc{'優惠價'}] = $ori_price_tmp[$i];
+    $coupon[$coupon_idx][$i] = 1;
+    $ori_price{$item_name[$i]} = $ori_price_tmp[$i];
+    $coupon_idx++;
+}
+$coupon_max = $coupon_idx;
+
+
+#合併後的內容另存
+for(my $i=0; $i<$coupon_max; $i++) {
+    for(my $j=0; $j<$item_max; $j++) {
+        $coupon_merge[$i][$j] = $coupon[$i][$j];
+    }
+}
+
 
 #印出所有優惠券內容
 my $coupon_html = "";
-$coupon_html .= "相關優惠：\n";
+$coupon_html .= "<h3>相關優惠 (含線上優惠券)</h3>\n";
 $coupon_html .= "<table border=1>\n";
-$coupon_html .= "<tr><th>券</th><th>優惠代碼</th><th>名稱</th><th>優惠價</th><th>有效期限</th><th>內容</th></tr>\n";
+$coupon_html .= "<tr><th>優惠代碼*</th><th>名稱</th><th>優惠價\$</th><th>有效期限</th><th>內容</th></tr>\n";
 
 open(FOUT, ">$coupon_txt") || die "錯誤！無法寫出所有優惠券內容到 $coupon_txt";
 print FOUT "#資料來源：https://goo.gl/7dL6yy\n";
@@ -121,31 +153,17 @@ for(my $i=0; $i<$coupon_idx; $i++) {
     print FOUT "$link\n" if( $link =~ /http/ );
     print FOUT "\n";
 
-    $link = "<a href=$link target=_blank>連結</a>" if( $link =~ /http/ );
+    $code_tmp = "<a href=$link target=_blank>$code_tmp</a>" if( $link =~ /http/ );
     $contain_tmp =~ s/、/<br>/g;
-    $coupon_html .= "<tr><td>$link</td><td><a name=\"$code_tmp\">$code_tmp</a></td><td>$name_tmp</td><td>$price_tmp</td><td>$vdate</td><td>$contain_tmp</td></tr>\n";
+    #valign=top這樣超連結跳過來時才可以看到後面所有
+    $coupon_html .= "<tr><td valign=top><a name=\"coupon$i\">$code_tmp</a></td><td valign=top>$name_tmp</td><td valign=top>$price_tmp</td><td valign=top>$vdate</td><td valign=top>$contain_tmp</td></tr>\n";
 }
 $coupon_html .= "</table>\n";
+$coupon_html .= "<font color=blue>*點擊優惠代碼以查看線上優惠券</font><br>\n";
+$coupon_html .= "<p>\n";
+$coupon_html .= "\n";
+
 close(FOUT);
-
-#單價列放最後才能補小細縫
-my @ori_price_tmp = split /,/, $ori_price_line;
-for(my $i=$item_1st_loc; $i<@item_name; $i++) {
-    $coupon[$coupon_idx][$item_loc{'名稱'}] = $item_name[$i];
-    $coupon[$coupon_idx][$item_loc{'優惠代碼'}] = "單點";
-    $coupon[$coupon_idx][$item_loc{'連結'}] = "na";
-    $coupon[$coupon_idx][$item_loc{'有效期間'}] = "na";
-    $coupon[$coupon_idx][$item_loc{'原價'}] = $ori_price_tmp[$i];
-    $coupon[$coupon_idx][$item_loc{'優惠價'}] = $ori_price_tmp[$i];
-    $coupon[$coupon_idx][$i] = 1;
-    $ori_price{$item_name[$i]} = $ori_price_tmp[$i];
-    $coupon_idx++;
-}
-
-$coupon_max = $coupon_idx;
-#print "DEBUG: coupon_max = $coupon_max\n";
-#print "DEBUG: \n";
-close(FIN);
 
 
 # 讀取需求檔 reqXXX.txt
@@ -159,21 +177,36 @@ my $line = 0;
 while(my $inbuf = <FIN>) {
     $line++;
     next if($inbuf =~ /^#/);
-    chomp $inbuf;
+    next if($inbuf =~ /^\s*$/);
+    #chomp $inbuf;
+    $inbuf =~ s/\r?\n$//;
     my @tmp = split /\s+/, $inbuf;
     next if( $tmp[0] == 0 );
     my $tmp_loc = $item_loc{$tmp[1]};
-    die "$req_file第$line錯誤！找不到 $tmp[1] 這品項" if( $tmp_loc < $item_1st_loc);
+    if( $tmp[1] =~ /^合併/ ) {
+        if( $tmp[1] eq $merge_words_chicken ) {
+            $merge_chicken = 1;
+        } elsif( $tmp[1] eq $merge_words_coketea) {
+            $merge_coketea = 1;
+        } else {
+            die "$req_file第$line行錯誤！錯誤的合併選項";
+        }
+        next;
+    }
+    die "$req_file第$line行錯誤！找不到 $tmp[1] 這品項" if( $tmp_loc < $item_1st_loc);
     $reqitem_cur[ $tmp_loc ] = $tmp[0];
 }
 close( FIN );
 
-open(FHTML, ">$output_html") || die "錯誤！開啟 $output_html 過程發生錯誤";
-print FHTML "需求清單：<br>\n";
-print FHTML "<table border=1>\n";
-print FHTML "<tr><th>品項</th><th>數量</th><th>單點原價</th><th>金額</th></tr>\n";
 
-print "目前日期時間：".localtime()."\n";
+open(FHTML, ">$output_html") || die "錯誤！開啟 $output_html 過程發生錯誤";
+print FHTML "日期時間：".localtime()."<br>\n";
+print FHTML "價格資料表版本：$csv_ver<br>\n";
+print FHTML "<h3>需求清單</h3>\n";
+print FHTML "<table border=1>\n";
+print FHTML "<tr><th>品項</th><th>數量</th><th>單價\$</th><th>金額\$</th></tr>\n";
+
+print "日期時間：".localtime()."\n";
 print "價格資料表版本：$csv_ver\n";
 print "\n";
 print "需求清單: (需求表檔名：$req_file)\n";
@@ -185,10 +218,8 @@ for(my $i=0; $i<@reqitem_cur; $i++) {
     if($reqitem_cur[$i]<0) {
         print "不要$item_name[$i]\n";
     } else {
-        for(my $j=0; $j<@coupon; $j++) {
-        }
         my $ssum = $ori_price{$item_name[$i]}*$reqitem_cur[$i];
-        print "$reqitem_cur[$i]*$item_name[$i] (單點原價:\$$ori_price{$item_name[$i]})\n";
+        print "$reqitem_cur[$i]*$item_name[$i] (單價:\$$ori_price{$item_name[$i]})\n";
         print FHTML "<tr><td>$item_name[$i]</td><td>$reqitem_cur[$i]</td><td>$ori_price{$item_name[$i]}</td><td>$ssum</td></tr>\n";
         $ori_price_total += $ssum;
         $ori_cnt += $reqitem_cur[$i];
@@ -197,16 +228,131 @@ for(my $i=0; $i<@reqitem_cur; $i++) {
 print "-------------------------\n";
 print "單點總價: \$$ori_price_total\n";
 print "\n";
+print "\n";
 
-
-print FHTML "<tr><th>總計</th><th>$ori_cnt</th><th></th><th>$ori_price_total</th></tr>\n";
+print FHTML "<tr><td><b>總計</b></td><td><b>$ori_cnt</b></td><td><b></b></td><td><b>$ori_price_total</b></td></tr>\n";
 print FHTML "</table>\n";
 print FHTML "<p>\n";
+print FHTML "\n";
 
 #初始化前n低的價格
 for(my $i=0; $i<$log_capacity+1; $i++) {
     $log_pool[$i] = $ori_price_total;
 }
+
+
+my $real_merge = 0;
+
+#處理合併
+if( $merge_chicken ) {
+    my @tmp_2bmerge = qw/咔啦脆雞(中辣) 上校薄脆雞 美式BBQ醬烤煙燻雞 醬香酸甜風味炸雞/;
+    my $cnt_merged = 0;
+
+    #合併需求
+    for( my $i=0; $i< @tmp_2bmerge; $i++ ) {
+        my $tmp_loc = $item_loc{ $tmp_2bmerge[$i] };
+        die "錯誤！找不到 $tmp_2bmerge[$i] 這品項" if( $tmp_loc < $item_1st_loc);
+        $cnt_merged += $reqitem_cur[ $tmp_loc ];
+        $reqitem_cur[ $tmp_loc ] = 0;
+    }
+
+    #若合併後>0才繼續做下一步
+    if( $cnt_merged > 0 ) {
+        $reqitem_cur[$item_max]=$cnt_merged;
+        for(my $c=0; $c<$coupon_max; $c++) {
+            for( my $i=0; $i< @tmp_2bmerge; $i++ ) {
+                my $tmp_loc = $item_loc{ $tmp_2bmerge[$i] };
+                die "錯誤！找不到 $tmp_2bmerge[$i] 這品項" if( $tmp_loc < $item_1st_loc);
+                if( $coupon_merge[$c][ $tmp_loc ] < 0 ) {
+                    $coupon_merge[$c][ $item_max ] = -1;
+                } elsif( $coupon_merge[$c][ $item_max ] >= 0 ) {
+                    $coupon_merge[$c][ $item_max ] += $coupon_merge[$c][ $tmp_loc ];
+                }
+                $coupon_merge[$c][ $tmp_loc ] = 0;
+            }
+        }
+        $item_name[$item_max] = $merge_words_chicken;
+        $item_name[$item_max] =~ s/：.*//;
+        $ori_price{$item_name[$item_max]} = $ori_price{'咔啦脆雞(中辣)'};
+        $item_max++;
+        $real_merge++;
+    }
+}
+
+#處理合併
+if( $merge_coketea ) {
+    my @tmp_2bmerge = qw/冰無糖茉莉綠茶 冰紅茶 百事可樂/;
+    my $cnt_merged = 0;
+
+    #合併需求
+    for( my $i=0; $i< @tmp_2bmerge; $i++ ) {
+        for(my $j=$item_1st_loc; $j<$item_max; $j++) {
+            if( $item_name[$j] =~ /$tmp_2bmerge[$i]/ ) {
+                $cnt_merged += $reqitem_cur[ $j ];
+                $reqitem_cur[ $j ] = 0;
+            }
+        }
+    }
+
+    #若合併後>0才繼續做下一步
+    if( $cnt_merged > 0 ) {
+        $reqitem_cur[$item_max]=$cnt_merged;
+        for(my $c=0; $c<$coupon_max; $c++) {
+            for( my $i=0; $i< @tmp_2bmerge; $i++ ) {
+                for(my $j=$item_1st_loc; $j<$item_max; $j++) {
+                    if( $item_name[$j] =~ /$tmp_2bmerge[$i]/ ) {
+                        if( $coupon_merge[$c][ $j ] < 0 ) {
+                            $coupon_merge[$c][ $item_max ] = -1;
+                        } elsif( $coupon_merge[$c][ $item_max ] >= 0 ) {
+                            $coupon_merge[$c][ $item_max ] += $coupon_merge[$c][ $j ];
+                        }
+                        $coupon_merge[$c][ $j ] = 0;
+                    }
+                }
+            }
+        }
+        $item_name[$item_max] = $merge_words_coketea;
+        $item_name[$item_max] =~ s/：.*//;
+        $ori_price{$item_name[$item_max]} = $ori_price{'百事可樂(小)'};
+        $item_max++;
+        $real_merge++;
+    }
+}
+
+#真的有合併的話，合併後的需求清單
+if( $real_merge ) {
+    print FHTML "<h3>合併後需求清單</h3>\n";
+    print FHTML "<table border=1>\n";
+    print FHTML "<tr><th>品項</th><th>數量</th></tr>\n";
+    
+    print "合併後需求清單:\n";
+    print "-------------------------\n";
+    my $ori_cnt = 0;
+    for(my $i=0; $i<@reqitem_cur; $i++) {
+        next if($reqitem_cur[$i]==0);
+        if($reqitem_cur[$i]<0) {
+            print "不要$item_name[$i]\n";
+        } else {
+            print "$reqitem_cur[$i]*$item_name[$i]\n";
+            print FHTML "<tr><td>$item_name[$i]</td><td>$reqitem_cur[$i]</td></tr>\n";
+            $ori_cnt += $reqitem_cur[$i];
+        }
+    }#
+    print "-------------------------\n";
+    print "$merge_words_warning\n";
+    print "$merge_words_chicken\n" if( $merge_chicken );
+    print "$merge_words_coketea\n" if( $merge_coketea );
+    
+    print FHTML "<tr><td><b>總計</b></td><td><b>$ori_cnt</b></td></tr>\n";
+    print FHTML "</table>\n";
+    print FHTML "<font color=red>$merge_words_warning</font><br>\n";
+    print FHTML "$merge_words_chicken<br>\n" if( $merge_chicken );
+    print FHTML "$merge_words_coketea<br>\n" if( $merge_coketea );
+    print FHTML "<p>\n";
+    print FHTML "\n";
+}
+
+
 
 #主要計算
 &compute_loop(0);
@@ -215,10 +361,18 @@ for(my $i=0; $i<$log_capacity+1; $i++) {
 #依價錢排序
 for( my $i=0; $i<$coupon_save_cnt; $i++) {
     $coupon_save_sort[$i] = $i;
+
+    for(my $j=0; $j<$coupon_max; $j++) {
+        $coupon_used[$j] = $coupon_saved[$i][$j];
+    }
+    $addition_save[$i] = &list_cnt(-2);
+    $addition_value_save[$i] = $addition_value;
 }
 for(my $i=0; $i<$coupon_save_cnt-1; $i++) {
     for(my $j=$i+1; $j<$coupon_save_cnt; $j++) {
-        if($cost_save[$coupon_save_sort[$i]] > $cost_save[$coupon_save_sort[$j]]) {
+        if( $cost_save[$coupon_save_sort[$i]] > $cost_save[$coupon_save_sort[$j]] ||
+            ($cost_save[$coupon_save_sort[$i]] == $cost_save[$coupon_save_sort[$j]] && $addition_value_save[$coupon_save_sort[$i]]<$addition_value_save[$coupon_save_sort[$j]])
+        ) {
             my $tmp = $coupon_save_sort[$i];
             $coupon_save_sort[$i] = $coupon_save_sort[$j];
             $coupon_save_sort[$j] = $tmp;
@@ -227,9 +381,9 @@ for(my $i=0; $i<$coupon_save_cnt-1; $i++) {
 }
 
 #印出排序結果
-print FHTML "計算結果：<br>\n";
+print FHTML "<h3>計算結果 (含線上優惠券)</h3>\n";
 print FHTML "<table border=1>\n";
-print FHTML "<tr><th>排名</th><th>總價</th><th>省下</th><th>組合</th><th>多了</th></tr>\n";
+print FHTML "<tr><th>排名</th><th>總價\$</th><th>省下\$</th><th>組合*</th><th>多了</th><th>多了\$</th></tr>\n";
 
 print "\n\n\n";
 my $double_cost_bound = $cost_save[$coupon_save_sort[0]]*2;
@@ -260,13 +414,11 @@ for(my $i=0; $i<$coupon_save_cnt; $i++) {
         my $used_coupon_cnt = $coupon_saved[$i_sort][$j];
         $coupon_used[$j] = $used_coupon_cnt;
         next if($used_coupon_cnt ==0 );
-        my $code_tmp = $coupon[$j][$item_loc{'優惠代碼'}];
-        $coupon_detail .= "[$code_tmp]*$used_coupon_cnt $coupon[$j][$item_loc{'名稱'}] \$$coupon[$j][$item_loc{'優惠價'}] -> ".&list_cnt( $j )."\n";
-        #if( $coupon[$j][$item_loc{'連結'}] =~ /http/ ) { $coupon_simple .= "<a href=$coupon[$j][$item_loc{'連結'}] target=_blank>"; }
-        if( $coupon[$j][$item_loc{'連結'}] =~ /http/ ) { $coupon_simple .= "<a href=#$code_tmp>"; }
-        $coupon_simple .= "[$code_tmp]";
-        if( $coupon[$j][$item_loc{'連結'}] =~ /http/ ) { $coupon_simple .= "</a>"; }
-        $coupon_simple .= "*$used_coupon_cnt $coupon[$j][$item_loc{'名稱'}] \$$coupon[$j][$item_loc{'優惠價'}]<br>";
+        my $code_tmp = $coupon_merge[$j][$item_loc{'優惠代碼'}];
+        my $link = $coupon_merge[$j][$item_loc{'連結'}];
+        $coupon_detail .= "[$code_tmp]*$used_coupon_cnt $coupon_merge[$j][$item_loc{'名稱'}] \$$coupon_merge[$j][$item_loc{'優惠價'}] -> ".&list_cnt( $j )."\n";
+        $code_tmp = "<a href=$link target=_blank>$code_tmp</a>" if( $link =~ /http/ );
+        $coupon_simple .= "[$code_tmp]*$used_coupon_cnt <a href=#coupon$j>$coupon_merge[$j][$item_loc{'名稱'}]</a> \$$coupon_merge[$j][$item_loc{'優惠價'}]<br>";
     }
 
     my $i1 = $i+1;
@@ -274,16 +426,21 @@ for(my $i=0; $i<$coupon_save_cnt; $i++) {
     print "#$i1 總價: \$$cost_save[$i_sort] -> ".&list_cnt(-1)."\n";
     print $coupon_detail;
     print "☆便宜\$$diff";
-    my $addition = &list_cnt(-2);
+
+    my $addition = $addition_save[$i_sort];
+    my $addition_value = $addition_value_save[$i_sort];
     print "(+$addition_value=".($ori_price_total-$cost_save[$i_sort]+$addition_value).")，還多了：$addition" if($addition ne "");
     print "\n\n";
 
     $addition =~ s/、/<br>/g;
-    print FHTML "<tr><td>$i1</td><td>$cost_save[$i_sort]</td><td>$diff</td><td>$coupon_simple</td><td>$addition</td></tr>\n";
+    $addition_value = "" if( $addition_value == 0 );
+    print FHTML "<tr><td>$i1</td><td>$cost_save[$i_sort]</td><td>$diff</td><td>$coupon_simple</td><td>$addition</td><td>$addition_value</td></tr>\n";
 }
 print FHTML "</table>\n";
+print FHTML "<font color=blue>*點擊優惠代碼以查看線上優惠券</font><br>\n";
 print FHTML "$ignore\n" if( $ignore ne "");
 print FHTML "<p>\n";
+print FHTML "\n";
 
 print FHTML $coupon_html;
 
@@ -294,6 +451,7 @@ my $etime = time;
 print "\n";
 print "計算時間".($etime-$stime)."秒\n";
 print "已產生優惠券內容：$coupon_txt\n";
+print "已產生結果網頁：$output_html\n";
 
 
 exit;
@@ -303,62 +461,63 @@ sub compute_loop {
     my $level = $_[0];
     my @reqitem_bak = @reqitem_cur;
     my $cost_bak = $cost_cur;
-    #my @coupon_used;
-
-    #print "\nDEBUG: level=$level ($coupon[$level][0]) / $coupon_max\n";
 
     if( $level == $coupon_max ) {
-        #check first if getting everything?
+        #先查是否買齊了
         for($item_chk=$item_1st_loc; $item_chk<$item_max; $item_chk++) {
             last if( $reqitem_cur[$item_chk] > 0);
         }
-        #print "DEBUG: last checked: $item_name[$item_
+
         if($item_chk==$item_max) {
             for(my $i=0; $i<$coupon_max; $i++) {
                 next if( $coupon_used[$i]==0 );
-                #print "DEBUG: [$coupon[$i][$item_loc{'優惠代碼'}]] $coupon[$i][$item_loc{'名稱'}] \$$coupon[$i][$item_loc{'優惠價'}] * $coupon_used[$i]\n";
+                #print "DEBUG: [$coupon_merge[$i][$item_loc{'優惠代碼'}]] $coupon_merge[$i][$item_loc{'名稱'}] \$$coupon_merge[$i][$item_loc{'優惠價'}] * $coupon_used[$i]\n";
                 $coupon_saved[$coupon_save_cnt][$i] = $coupon_used[$i];
             }
-            #print "DEBUG: Total price: \$$cost_cur -> ";
             $cost_save[$coupon_save_cnt] = $cost_cur;
             $coupon_save_cnt++;
-            #print "DEBUG: ".&list_cnt(-1);
-            #print "DEBUG: \n\n";
 
-            # maintain price pool
+            #減少時間：只算前幾個便宜的
             push @log_pool, $cost_cur;
             @log_pool = sort {$a <=> $b} @log_pool;
-            #print "DEBUG: $cost_cur, $log_pool[$log_capacity]\n";
+        #} else {
+        #    print "DEBUG 買不齊: item_name[$item_chk]=$item_name[$item_chk] reqitem_cur[$item_chk]=$reqitem_cur[$item_chk] >0\n";
         }
         return;
     } else {
-        #print "DEBUG: found range\n";
+        #計算最多幾組
         my $range_max=0;
         for(my $i=$item_1st_loc; $i<$item_max; $i++) {
-            next if($coupon[$level][$i]==0);
+            next if($coupon_merge[$level][$i]==0);
             if($reqitem_cur[$i]<0) {
                 $range_max = 0;
                 last;
             }
-            my $range_this = ($reqitem_cur[$i]-($reqitem_cur[$i]%$coupon[$level][$i])) / $coupon[$level][$i];
-            $range_this++ if( ($reqitem_cur[$i]%$coupon[$level][$i]) > 0);
+
+            my $range_this = ($reqitem_cur[$i]-($reqitem_cur[$i]%$coupon_merge[$level][$i])) / $coupon_merge[$level][$i];
+            $range_this++ if( ($reqitem_cur[$i]%$coupon_merge[$level][$i]) > 0);
+            #if( $range_this > 0 ) {
+            #    print "DEBUG C: i=$i, item_name[$i]=$item_name[$i], reqitem_cur[$i]=$reqitem_cur[$i], coupon[$level][$i]=$coupon[$level][$i]\n";
+            #}
             $range_max = $range_this if( $range_max < $range_this );
         }
 
         for(my $i=0; $i<=$range_max; $i++) {
-            my $cost_tmp = $cost_bak + $coupon[$level][$item_loc{'優惠價'}]*$i;
-            last if( $cost_tmp>$ori_price_total || $cost_tmp>$log_pool[$log_capacity] );
+            my $cost_tmp = $cost_bak + $coupon_merge[$level][$item_loc{'優惠價'}]*$i;
+            if( $cost_tmp>$ori_price_total || $cost_tmp>$log_pool[$log_capacity] ) {
+                #print "DEBUG: price $cost_tmp>$ori_price_total || $cost_tmp>$log_pool[$log_capacity]\n";
+                last;
+            }
 
             $coupon_used[$level] = $i;
             for(my $j=$item_1st_loc; $j<$item_max; $j++) {
-                $reqitem_cur[$j] -= $coupon[$level][$j]*$i;
+                $reqitem_cur[$j] -= $coupon_merge[$level][$j]*$i;# if($coupon_merge[$level][$j]>0);
             }
             $cost_cur = $cost_tmp;
-            #print "DEBUG: $level/$coupon_max: ".(" "x$level)."$coupon[$level][$item_loc{'名稱'}] range = $i/$range_max\n";
+            #print "DEBUG: $level/$coupon_max: ".(" "x$level)."$coupon[$level][$item_loc{'名稱'}] $coupon[$level][$item_loc{'優惠代碼'}] range = $i/$range_max\n";
             &compute_loop( $level+1 );
             @reqitem_cur = @reqitem_bak;
         }
-        #print "DEBUG: done\n";
     }
 }
 
@@ -372,16 +531,20 @@ sub list_cnt {
     my $first = 1;
     my $ret = "";
     $addition_value=0;
+
     for(my $i=$item_1st_loc; $i<$item_max; $i++) {
         my $cnt=0;
         if( $specific_coupon < 0 ) {
             for(my $j=0; $j<$coupon_max; $j++) {
-                $cnt += $coupon[$j][$i]*$coupon_used[$j];
+                $cnt += $coupon_merge[$j][$i]*$coupon_used[$j];
             }
             $cnt-=$reqitem_cur[$i] if($specific_coupon<-1 && $reqitem_cur[$i]>0);
-        } else {
-            $cnt += $coupon[$specific_coupon][$i];
+        } else { #單獨的coupon內容就用合併前的coupon
+            my $tmp = $coupon[$specific_coupon][$i];
+            $tmp = -$tmp if( $tmp<0 );
+            $cnt += $tmp;
         }
+
         if( $cnt>0 ) {
             if($first) {
                 $first=0;
